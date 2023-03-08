55ffd64cdd33b73e6e87750bf0b397be7b03145e
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-03-08 13:31:18 -0800

    moved directory

--- Assignments/R/mps_conv_function.R
+++
@@ -1,9 +0,0 @@
-# Speed in meters per second
-
-#' @param kph: The input speed in kilometers per hour
-#' @return mps: The output speed in meters per second
-
-mps_conv <- function(kph) {
-  mps <- (kph*1000)/3600
-  return(mps)
-}
